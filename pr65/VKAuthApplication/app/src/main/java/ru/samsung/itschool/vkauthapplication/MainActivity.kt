c13425e766530abcef5b5a3bd2cab6c559a2afe5
KostinAO <kostin.a.o@edu.narfu.ru>
2022-06-05 13:33:44 +0300

    first commit

---
+++ pr65/VKAuthApplication/app/src/main/java/ru/samsung/itschool/vkauthapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package ru.samsung.itschool.vkauthapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
+import ru.samsung.itschool.vkauthapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btLogin.setOnClickListener({
+            VK.login(this, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        })
+
+        binding.btLogoff.setOnClickListener({
+            VK.logout()
+            if(!VK.isLoggedIn()) binding.tvRes.text="Not login to VK"
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(!VK.isLoggedIn()) binding.tvRes.text="Not login to VK"
+        else binding.tvRes.text="Now logged in to VK"
+    }
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                Toast.makeText(applicationContext, token.toString(), Toast.LENGTH_LONG).show()
+                binding.tvRes.text="Login success. ID: "+token.userId
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+                binding.tvRes.text="Login fail"
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
